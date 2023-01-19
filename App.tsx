84709fb28c296ee8c279ccf4013b50a6595eff4b
felipeperaltacheung <f33___@hotmail.com>
2023-01-19 15:41:38 -0300

    Fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+  useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>PAGA A CANE</Text>
+      <Text style={styles.text}>rrrr </Text>
       <StatusBar style="auto" />
     </View>
   );
